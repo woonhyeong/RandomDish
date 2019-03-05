df423f3cac0368409b27d3c441f8b06848ae9989
woonhyeong <tlsehddkql12@gmail.com>
2019-03-05 22:42:50 +0900

    Start
    기본적인 클래스와 버튼 생성

---
+++ DishesPocket/DishesBrain.swift
@@ -0,0 +1,14 @@
+//
+//  DishesBrain.swift
+//  DishesPocket
+//
+//  Created by 이운형 on 05/03/2019.
+//  Copyright © 2019 201302458. All rights reserved.
+//
+
+import Foundation
+
+class DishesBrain {
+    var dishList = [String]()
+    
+}
---
+++ DishesPocket/DishesViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DishesPocket
+//
+//  Created by 이운형 on 05/03/2019.
+//  Copyright © 2019 201302458. All rights reserved.
+//
+
+import UIKit
+
+class DishesViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var dishesAddButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
