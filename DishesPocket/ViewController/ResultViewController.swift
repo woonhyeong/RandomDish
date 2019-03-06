7e0328b4a3cdb6a87c51f4693d91593fc9cf198d
woonhyeong <tlsehddkql12@gmail.com>
2019-03-06 19:44:59 +0900

    Add IBOutlet & Autolayout  to ResultViewController
    ResultViewController의 image와 label을 추가하는 addResultImage(), addResultText() 메소드를 추가했고, autolayout을 코드로 설정했다.

--- DishesPocket/ViewController/ResultViewController.swift
+++ DishesPocket/ViewController/ResultViewController.swift
@@ -13,4 +13,55 @@ class ResultViewController : UIViewController {
     @IBOutlet weak var memoLabel: UILabel!
     @IBOutlet weak var memoImage: UIImageView?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addResultImage()
+        addResultText()
+    }
+    
+    func addResultImage() {
+        let image: UIImageView = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(image)
+        if let memoImage = UIImage(named: "sticker") {
+            image.image = memoImage
+        }
+        
+        let centerX : NSLayoutConstraint = image.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        let centerY : NSLayoutConstraint = image.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        
+        let width : NSLayoutConstraint = image.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5)
+        let height : NSLayoutConstraint = image.heightAnchor.constraint(equalTo: image.widthAnchor, multiplier: 1)
+        
+        centerX.isActive = true
+        centerY.isActive = true
+        width.isActive = true
+        height.isActive = true
+        
+        memoImage = image
+    }
+    
+    func addResultText() {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        memoImage?.addSubview(label)
+        
+        guard let image = memoImage else {
+            return
+        }
+        
+        let centerX : NSLayoutConstraint = label.centerXAnchor.constraint(equalTo: image.centerXAnchor)
+        let centerY : NSLayoutConstraint = label.centerYAnchor.constraint(equalTo: image.centerYAnchor)
+        let width : NSLayoutConstraint = label.widthAnchor.constraint(equalTo: image.widthAnchor, multiplier: 0.8)
+        let height : NSLayoutConstraint =  label.heightAnchor.constraint(equalTo: image.widthAnchor, multiplier: 0.1)
+        
+        centerX.isActive = true
+        centerY.isActive = true
+        width.isActive = true
+        height.isActive = true
+        
+        memoLabel = label
+    }
 }
